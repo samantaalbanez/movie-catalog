feb67569667d8d8b704a017224d24b93342fe2ff
Samanta Albanez <samantaalbanez@MacBook-Pro-de-Samanta.local>
2026-07-23 11:37:05 -0300

    feat: add errorScreen and loadingScreen, add Interceptor to Language

---
+++ app/src/main/java/br/com/samantaalbanez/moviescatalog/core/network/LanguageInterceptor.kt
@@ -0,0 +1,40 @@
+package br.com.samantaalbanez.moviescatalog.core.network
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.util.Locale
+import javax.inject.Inject
+
+internal class LanguageInterceptor @Inject constructor() : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val originalUrl = originalRequest.url
+
+        val newUrl = originalUrl.newBuilder()
+            .addQueryParameter(LANGUAGE_PARAM, getDeviceLanguage())
+            .build()
+
+        val newRequest = originalRequest.newBuilder()
+            .url(newUrl)
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+
+    private fun getDeviceLanguage(): String {
+        val locale = Locale.getDefault()
+        val language = locale.language
+        val country = locale.country
+
+        return if (country.isNotBlank()) {
+            "$language-$country"
+        } else {
+            language
+        }
+    }
+
+    companion object {
+        private const val LANGUAGE_PARAM = "language"
+    }
+}
---
+++ app/src/main/java/br/com/samantaalbanez/moviescatalog/ui/components/ErrorScreen.kt
@@ -0,0 +1,34 @@
+package br.com.samantaalbanez.moviescatalog.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+internal fun ErrorScreen(
+    message: String,
+    onRetry: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center
+    ) {
+        Text(text = message, color = MaterialTheme.colorScheme.error)
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onRetry) {
+            Text(text = "Tentar Novamente")
+        }
+    }
+}
