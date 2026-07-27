57f08985bf00bc1bb7a03359b79bdd99f617c314
Samanta Albanez <samantaalbanez@MacBook-Pro-de-Samanta.local>
2026-07-27 19:53:37 -0300

    fix: unit test

--- app/src/test/java/br/com/samantaalbanez/moviescatalog/data/remote/interceptor/AuthenticationInterceptorTest.kt
+++ app/src/test/java/br/com/samantaalbanez/moviescatalog/data/remote/interceptor/AuthenticationInterceptorTest.kt
@@ -11,8 +11,10 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [33])
 internal class AuthenticationInterceptorTest {
 
     private val interceptor = AuthenticationInterceptor()
