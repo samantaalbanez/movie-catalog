ab5f539720cb85d25b62b3fc72973ffc557d87fe
Samanta Albanez <samantaalbanez@MacBook-Pro-de-Samanta.local>
2026-07-27 15:33:10 -0300

    fix: unit tests

--- app/src/test/java/br/com/samantaalbanez/moviescatalog/di/NetworkModuleTest.kt
+++ app/src/test/java/br/com/samantaalbanez/moviescatalog/di/NetworkModuleTest.kt
@@ -1,17 +1,31 @@
 package br.com.samantaalbanez.moviescatalog.di
 
+import android.content.Context
 import br.com.samantaalbanez.moviescatalog.BuildConfig
 import br.com.samantaalbanez.moviescatalog.data.remote.interceptor.AuthenticationInterceptor
 import br.com.samantaalbanez.moviescatalog.data.remote.interceptor.LanguageInterceptor
+import io.mockk.every
+import io.mockk.mockk
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import retrofit2.Retrofit
+import java.io.File
 
 internal class NetworkModuleTest {
+
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        context = mockk(relaxed = true) {
+            every { cacheDir } returns File(System.getProperty("java.io.tmpdir"), "http_cache")
+        }
+    }
 
     @Test
     fun `given required interceptors when provideOkHttpClient is called then return client containing all interceptors`() {
@@ -24,7 +38,8 @@ internal class NetworkModuleTest {
         val result = NetworkModule.provideOkHttpClient(
             authInterceptor = authInterceptor,
             languageInterceptor = languageInterceptor,
-            loggingInterceptor = loggingInterceptor
+            loggingInterceptor = loggingInterceptor,
+            context = context
         )
 
         // Then
